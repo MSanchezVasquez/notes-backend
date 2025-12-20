77a30846a5c1398d80a6631ba7a872c4bd637578
Moises Sanchez <140144292+MSanchezVasquez@users.noreply.github.com>
2025-12-20 08:19:25 -0500

    feat: cambiar la funcionalidad de agregar notes del backed para usar la base de datos

--- index.js
+++ index.js
@@ -63,21 +63,18 @@ const generateId = () => {
 app.post("/api/notes", (request, response) => {
   const body = request.body;
 
-  if (!body.content) {
-    return response.status(400).json({
-      error: "content missing",
-    });
+  if (body.content === undefined) {
+    return response.status(400).json({ error: "content missing" });
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
-    important: Boolean(body.important) || false,
-    id: generateId(),
-  };
+    important: body.important || false,
+  });
 
-  notes = notes.concat(note);
-
-  response.json(note);
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
 });
 
 app.put("/api/notes/:id", (request, response) => {
