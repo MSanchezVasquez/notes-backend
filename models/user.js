930dbf3228178e14e8dbe9950eed745a02a07b9b
Moises Sanchez <140144292+MSanchezVasquez@users.noreply.github.com>
2026-01-06 19:54:16 -0500

    feat: creando usuarios

--- models/user.js
+++ models/user.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  username: String,
+  username: {
+    type: String,
+    required: true,
+    unique: true, // esto asegura la unicidad de username
+  },
   name: String,
   passwordHash: String,
   notes: [
